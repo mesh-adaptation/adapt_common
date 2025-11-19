f53d532bbac2575be2393e38b4d4a3dd1e69d3a8
Joe Wallwork <jw2423@cam.ac.uk>
2025-11-19 08:44:13 +0000

    Unit tests for cofunction2function

---
+++ test/test_utility_conversion.py
@@ -0,0 +1,35 @@
+"""Test the conversions between Cofunctions and Functions in the utility module."""
+
+import firedrake as fd
+import numpy as np
+import pytest
+
+from adapt_common.utility import cofunction2function
+
+
+def test_cofunction2function_function_space():
+    """Test that the converted Function has the expected function space."""
+    V = fd.FunctionSpace(fd.UnitSquareMesh(1, 1), "CG", 1)
+    cofunc = fd.Cofunction(V.dual())
+    func = cofunction2function(cofunc)
+    assert func.function_space() == V
+    assert func.function_space().dual() == cofunc.function_space()
+
+
+def test_cofunction2function_data():
+    """Test that the data in the Cofunction is correctly transferred to the Function."""
+    V = fd.FunctionSpace(fd.UnitSquareMesh(4, 4), "CG", 1)
+    cofunc = fd.Cofunction(V.dual())
+    cofunc.dat.data[:] = np.arange(len(cofunc.dat.data))
+    func = cofunction2function(cofunc)
+    assert np.allclose(func.dat.data, cofunc.dat.data)
+
+
+@pytest.mark.parallel(nprocs=[2, 3])
+def test_cofunction2function_data_with_halos():
+    """Test that the Cofunction data with halos is correctly transferred."""
+    V = fd.FunctionSpace(fd.UnitSquareMesh(16, 16), "CG", 1)
+    cofunc = fd.Cofunction(V.dual())
+    cofunc.dat.data_with_halos[:] = np.arange(len(cofunc.dat.data_with_halos))
+    func = cofunction2function(cofunc)
+    assert np.allclose(func.dat.data_with_halos, cofunc.dat.data_with_halos)
