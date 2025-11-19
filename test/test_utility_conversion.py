f6d2a5b496e3fd14f2245b56472ffbbb3b67b00d
Joe Wallwork <jw2423@cam.ac.uk>
2025-11-19 09:10:37 +0000

    Account for mixed spaces; set random data

--- test/test_utility_conversion.py
+++ test/test_utility_conversion.py
@@ -7,55 +7,68 @@ import pytest
 from adapt_common.utility import cofunction2function, function2cofunction
 
 
-def test_cofunction2function_function_space():
+@pytest.fixture(params=["simple", "mixed"])
+def function_space(request):
+    """Return a function space for testing."""
+    V = fd.FunctionSpace(fd.UnitSquareMesh(12, 12), "CG", 1)
+    return V if request.param == "simple" else fd.MixedFunctionSpace([V, V])
+
+
+def set_random_data(function_or_cofunction):
+    """Set random data in the given Function or Cofunction."""
+    rng = np.random.default_rng()
+    if isinstance(function_or_cofunction.function_space().dof_count, np.int64):
+        size = len(function_or_cofunction.dat.data_with_halos)
+        function_or_cofunction.dat.data_with_halos[:] = rng.random(size)
+    else:
+        for i, data in enumerate(function_or_cofunction.dat.data_with_halos):
+            function_or_cofunction.dat.data_with_halos[i][:] = rng.random(len(data))
+    assert not np.allclose(function_or_cofunction.dat.data_with_halos, 0)
+
+
+def test_cofunction2function_function_space(function_space):
     """Test that the converted Function has the expected function space."""
-    V = fd.FunctionSpace(fd.UnitSquareMesh(1, 1), "CG", 1)
-    cofunc = fd.Cofunction(V.dual())
+    cofunc = fd.Cofunction(function_space.dual())
     func = cofunction2function(cofunc)
-    assert func.function_space() == V
+    assert func.function_space() == function_space
     assert func.function_space().dual() == cofunc.function_space()
 
 
-def test_cofunction2function_data():
+def test_cofunction2function_data(function_space):
     """Test that the data in the Cofunction is correctly transferred to the Function."""
-    V = fd.FunctionSpace(fd.UnitSquareMesh(4, 4), "CG", 1)
-    cofunc = fd.Cofunction(V.dual())
-    cofunc.dat.data[:] = np.arange(len(cofunc.dat.data))
+    cofunc = fd.Cofunction(function_space.dual())
+    set_random_data(cofunc)
     func = cofunction2function(cofunc)
     assert np.allclose(func.dat.data, cofunc.dat.data)
 
 
 @pytest.mark.parallel(nprocs=[2, 3])
-def test_cofunction2function_data_with_halos():
+def test_cofunction2function_data_with_halos(function_space):
     """Test that the Cofunction data with halos is correctly transferred."""
-    V = fd.FunctionSpace(fd.UnitSquareMesh(16, 16), "CG", 1)
-    cofunc = fd.Cofunction(V.dual())
-    cofunc.dat.data_with_halos[:] = np.arange(len(cofunc.dat.data_with_halos))
+    cofunc = fd.Cofunction(function_space.dual())
+    set_random_data(cofunc)
     func = cofunction2function(cofunc)
     assert np.allclose(func.dat.data_with_halos, cofunc.dat.data_with_halos)
 
 
-def test_function2cofunction_function_space():
+def test_function2cofunction_function_space(function_space):
     """Test that the converted Cofunction has the expected function space."""
-    V = fd.FunctionSpace(fd.UnitSquareMesh(1, 1), "CG", 1)
-    cofunc = function2cofunction(fd.Function(V))
-    assert V.dual() == cofunc.function_space()
+    cofunc = function2cofunction(fd.Function(function_space))
+    assert function_space.dual() == cofunc.function_space()
 
 
-def test_function2cofunction_data():
+def test_function2cofunction_data(function_space):
     """Test that the data in the Function is correctly transferred to the Cofunction."""
-    V = fd.FunctionSpace(fd.UnitSquareMesh(4, 4), "CG", 1)
-    func = fd.Function(V)
-    func.dat.data[:] = np.arange(len(func.dat.data))
+    func = fd.Function(function_space)
+    set_random_data(func)
     cofunc = function2cofunction(func)
     assert np.allclose(func.dat.data, cofunc.dat.data)
 
 
 @pytest.mark.parallel(nprocs=[2, 3])
-def test_function2cofunction_data_with_halos():
+def test_function2cofunction_data_with_halos(function_space):
     """Test that the Function data with halos is correctly transferred."""
-    V = fd.FunctionSpace(fd.UnitSquareMesh(16, 16), "CG", 1)
-    func = fd.Function(V)
-    func.dat.data_with_halos[:] = np.arange(len(func.dat.data_with_halos))
+    func = fd.Function(function_space)
+    set_random_data(func)
     cofunc = function2cofunction(func)
     assert np.allclose(func.dat.data_with_halos, cofunc.dat.data_with_halos)
